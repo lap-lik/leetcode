58726d756681c1a5d81d0f68347e81367099aeee
Kokhno Anton <70767412+lap-lik@users.noreply.github.com>
2024-03-01 20:29:29 +0300

    Time: 0 ms (100.00%), Space: 42.8 MB (55.18%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,19 +3,13 @@ class Solution {
         int res = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target){
-                return res;
-            }
-            if (nums[i] < target){
-                res++;
-                if (i == nums.length-1){
-                    return res++;
-                }
+                return i;
             }
             if (nums[i] > target){
-                return res;
+                return i;
             }
+            res++;
         }
-        res++;
         return res;
     }
 }
