2026d4e6e12c7ef7bdace3fbdd4f4727179cc9cb
Kokhno Anton <70767412+lap-lik@users.noreply.github.com>
2024-03-02 23:51:01 +0300

    Time: 6 ms (38.63%), Space: 43.7 MB (89.20%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String num = "" + x;
+        String num = String.valueOf(x);
         for (int i = 0, j = num.length()-1; i < num.length(); i++, j--) {
             if (num.charAt(i) != num.charAt(j)){
                 return false;
