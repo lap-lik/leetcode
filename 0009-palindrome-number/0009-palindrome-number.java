1567f8d912dc86ae74bbf8b93b9d19239914d08d
Kokhno Anton <70767412+lap-lik@users.noreply.github.com>
2024-03-02 23:47:52 +0300

    Time: 6 ms (38.63%), Space: 43.7 MB (89.20%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,7 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String num = String.valueOf(x);
-        StringBuilder numRevers = new StringBuilder(num).reverse();
-        return num.equals(numRevers.toString());
+        String num = "" + x;
+        for (int i = 0, j = num.length()-1; i < num.length(); i++, j--) {
+            if (num.charAt(i) != num.charAt(j)){
+                return false;
+            }
+        }
+        return true;
     }
 }
