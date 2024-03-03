2bd40d3ecdbd36656704d924d97c33cb73bc8d65
Kokhno Anton <70767412+lap-lik@users.noreply.github.com>
2024-03-03 03:25:02 +0300

    Time: 19 ms (11.47%), Space: 41 MB (33.80%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,6 +1,30 @@
 class Solution {
     public int mySqrt(int x) {
 
-        return (int) Math.sqrt(x);
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        long res = x / 2;
+
+        while (true) {
+            long temp = res * res;
+            if (temp == x) {
+                break;
+            }
+            if (temp > x){
+                res = res/2;
+            }
+            if (temp < x){
+                res++;
+                if (res*res == x){
+                    break;
+                }
+                if (res*res > x){
+                    res--;
+                    break;
+                }
+            }
+        }
+        return (int) res;
     }
 }
