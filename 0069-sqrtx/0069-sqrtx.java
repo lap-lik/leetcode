fb8370b4c88a651a8e69f9aba88d345ce379e1b5
Kokhno Anton <70767412+lap-lik@users.noreply.github.com>
2024-03-03 03:26:20 +0300

    Time: 1 ms (86.43%), Space: 40.9 MB (45.52%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -4,27 +4,10 @@ class Solution {
         if (x == 0 || x == 1) {
             return x;
         }
-        long res = x / 2;
-
-        while (true) {
-            long temp = res * res;
-            if (temp == x) {
-                break;
-            }
-            if (temp > x){
-                res = res/2;
-            }
-            if (temp < x){
-                res++;
-                if (res*res == x){
-                    break;
-                }
-                if (res*res > x){
-                    res--;
-                    break;
-                }
-            }
+        long temp = x;
+        while (temp * temp > x) {
+            temp = ((temp + x / temp) / 2);
         }
-        return (int) res;
+        return (int)temp;
     }
 }
