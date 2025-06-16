2afb4a47996f75d41c231d9c47aff7da7392469a
Kokhno Anton <70767412+lap-lik@users.noreply.github.com>
2025-06-17 01:35:10 +0300

    Time: 0 ms (100%), Space: 41.2 MB (53.84%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,45 +15,53 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null){
+        // if(p == null && q == null){
+        //     return true;
+        // }
+        // if((p == null && q != null) || (p != null && q == null)){
+        //     return false;
+        // }
+        // Queue<TreeNode> queueP = new LinkedList<>();
+        // queueP.add(p);
+        // Queue<TreeNode> queueQ = new LinkedList<>();
+        // queueQ.add(q);
+        // while(!queueP.isEmpty()){
+        //     TreeNode nodeP = queueP.poll();
+        //     TreeNode nodeQ = queueQ.poll();
+        //     if(nodeP.val != nodeQ.val){
+        //         return false;
+        //     }
+
+        //     TreeNode nodePLeft = nodeP.left;
+        //     TreeNode nodeQLeft = nodeQ.left;
+        //     if ((nodePLeft == null && nodeQLeft != null) || (nodePLeft != null && nodeQLeft == null)){
+        //         return false;
+        //     } else if(nodePLeft != null && nodeQLeft != null && nodePLeft.val != nodeQLeft.val) {
+        //         return false;
+        //     } else if(nodePLeft != null && nodeQLeft != null && nodePLeft.val == nodeQLeft.val) {
+        //         queueP.add(nodePLeft);
+        //         queueQ.add(nodeQLeft);         
+        //     }
+
+        //     TreeNode nodePRight = nodeP.right;
+        //     TreeNode nodeQRight = nodeQ.right;
+        //     if ((nodePRight == null && nodeQRight != null) || (nodePRight != null && nodeQRight == null)){
+        //         return false;
+        //     } else if(nodePRight != null && nodeQRight != null && nodePRight.val != nodeQRight.val) {
+        //         return false;
+        //     } else if(nodePRight != null && nodeQRight != null && nodePRight.val == nodeQRight.val) {
+        //         queueP.add(nodePRight);
+        //         queueQ.add(nodeQRight);         
+        //     }
+        // }
+        // return true;
+
+        if(p== null && q == null) {
             return true;
+        } else if(p != null && q!=null && p.val == q.val) {
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
         }
-        if((p == null && q != null) || (p != null && q == null)){
-            return false;
-        }
-        Queue<TreeNode> queueP = new LinkedList<>();
-        queueP.add(p);
-        Queue<TreeNode> queueQ = new LinkedList<>();
-        queueQ.add(q);
-        while(!queueP.isEmpty()){
-            TreeNode nodeP = queueP.poll();
-            TreeNode nodeQ = queueQ.poll();
-            if(nodeP.val != nodeQ.val){
-                return false;
-            }
 
-            TreeNode nodePLeft = nodeP.left;
-            TreeNode nodeQLeft = nodeQ.left;
-            if ((nodePLeft == null && nodeQLeft != null) || (nodePLeft != null && nodeQLeft == null)){
-                return false;
-            } else if(nodePLeft != null && nodeQLeft != null && nodePLeft.val != nodeQLeft.val) {
-                return false;
-            } else if(nodePLeft != null && nodeQLeft != null && nodePLeft.val == nodeQLeft.val) {
-                queueP.add(nodePLeft);
-                queueQ.add(nodeQLeft);         
-            }
-
-            TreeNode nodePRight = nodeP.right;
-            TreeNode nodeQRight = nodeQ.right;
-            if ((nodePRight == null && nodeQRight != null) || (nodePRight != null && nodeQRight == null)){
-                return false;
-            } else if(nodePRight != null && nodeQRight != null && nodePRight.val != nodeQRight.val) {
-                return false;
-            } else if(nodePRight != null && nodeQRight != null && nodePRight.val == nodeQRight.val) {
-                queueP.add(nodePRight);
-                queueQ.add(nodeQRight);         
-            }
-        }
-        return true;
+        return false;
     }
 }
